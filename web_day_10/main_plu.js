ce847025f7c9c9a9f3dac61d1b21745aad273995
Vlada <polovinska.v@gmail.com>
2018-09-03 19:48:52 +0300

    day1_create

--- web_day_10/main_plu.js
+++ web_day_10/main_plu.js
@@ -1,10 +1,10 @@
 
   $('#date').datepicker({
     showOn: "button",
-    buttonImage: " /Users/vladazukova/Desktop/programming/rcs/calendar.png",
+    buttonImage: "/Users/vladazukova/Desktop/programming/rcs/calendar.png",
     buttonImageOnly: true,
     buttonText: "Select date",
-innerWidth: 5
+    
 
   });
  
---
+++ web_day_11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int vesels = 5;
+            double arKomatu = 4.5;
+            String simboluVirkne = "defincija";
+            char simvols = 'f';
+            bool patiesaVert = true;
+            vesels = 6;
+            simboluVirkne = Console.ReadLine();
+            Console.WriteLine(simboluVirkne);
+            Console.ReadLine();
+
+
+                    }
+    }
+}
